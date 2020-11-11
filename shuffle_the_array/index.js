38c5eda2bfdb6fc92101143a0fe9889257fda41c
Justin Graber <justin.graber@equipmentshare.com>
2020-11-11 14:26:59 -0600

    shuffle_the_array

---
+++ shuffle_the_array/index.js
@@ -0,0 +1,65 @@
+/*
+Shuffle the Array
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Example 1:
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+Example 2:
+
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1]
+Example 3:
+
+Input: nums = [1,1,2,2], n = 2
+Output: [1,2,1,2]
+ 
+
+Constraints:
+
+1 <= n <= 500
+nums.length == 2n
+1 <= nums[i] <= 10^3
+*/
+
+/*
+ * Param nums: array of integers - nums.length == 2n
+ * Param n: integer - 1 <= n <= 500
+ */
+
+const assertEqual = (arrA, arrB) => {
+    if (arrA.length !== arrB.length) {
+        return false;
+    }
+
+    for (let i=0; i < arrA.length; i++) {
+        if (arrA[i] !== arrB[i]) {
+            throw new Error('arrays are not equal');
+        }
+    }
+};
+
+const shuffle = (nums, n) => {
+    if (n >= nums.length) {
+        return nums;
+    }
+
+    const ret = new Array(nums.length);
+
+    for (let i=0; i < nums.length / 2; i++) {
+        currIdx = i * 2;
+        ret[currIdx] = nums[i];
+        ret[currIdx+1] = nums[i+n];
+    }
+
+    return ret;
+};
+
+
+assertEqual(shuffle([2,5,1,3,4,7], 3), [2,3,5,4,1,7]);
+assertEqual(shuffle([1,2,3,4,4,3,2,1], 4), [1,4,2,3,3,2,4,1]);
+assertEqual(shuffle([1,1,2,2], 2), [1,2,1,2]);
